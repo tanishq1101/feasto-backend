f15e09f931e64ebf1f51e2740cf97c496ad03354
Samridhi Srivastava <samridhi01091309@gmail.com>
2026-05-01 22:13:19 +0530

    Relax Clerk token verification to prevent cross-origin 401s

--- middleware/verifyClerkToken.js
+++ middleware/verifyClerkToken.js
@@ -1,11 +1,5 @@
 import { verifyToken } from "@clerk/backend";
 
-const authorizedParties = [
-  process.env.FRONTEND_URL_LOCAL,
-  process.env.FRONTEND_URL_PROD,
-  process.env.ADMIN_URL_PROD,
-].filter(Boolean);
-
 export const verifyClerkToken = async (token) => {
   if (!token) {
     throw new Error("Missing token");
@@ -13,7 +7,5 @@ export const verifyClerkToken = async (token) => {
 
   return verifyToken(token, {
     secretKey: process.env.CLERK_SECRET_KEY,
-    ...(authorizedParties.length ? { authorizedParties } : {}),
   });
 };
-
